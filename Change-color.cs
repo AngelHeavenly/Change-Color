3b4663695f511d674ab02e7bbd5f0b50843280ba
AngelHeavenly <ilya.tsinovkin@mail.ru>
2023-05-21 11:54:42 +1000

    Create Change-color.cs

---
+++ Change-color.cs
@@ -0,0 +1,6 @@
+var asset = ToolsModifierControl.toolController.m_editPrefabInfo as VehicleInfo;
+
+asset.m_color0 = new Color32(82, 93, 105, 1);
+asset.m_color1 = new Color32(159, 94, 94, 1);
+asset.m_color2 = new Color32(106, 148, 100, 1);
+asset.m_color3 = new Color32(139, 136, 100, 1);
